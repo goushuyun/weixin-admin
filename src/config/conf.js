70649ed2f2e7c48aa2e00b30d02f6898da2c14cb
Wang-Kai <persistence2013@sina.com>
2017-06-02 11:22:12 +0800

    add url_encode_domain at conf.js

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,5 +1,6 @@
 // 生产环境
 // export default {
+//     url_encode_domain: 'https%3a%2f%2fapp.goushuyun.com',
 //     base_url: 'http://admin.goushuyun.com/',
 //     home_href: 'http://admin.goushuyun.com/#'
 // }
