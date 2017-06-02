56be70a1d99a4e3aeccab57660613a4828f8bb5b
Wang-Kai <persistence2013@sina.com>
2017-06-02 14:52:16 +0800

    动态切换生产、测试环境下的 component_appid

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,12 +1,14 @@
 // 生产环境
-// export default {
-//     url_encode_domain: 'https%3a%2f%2fapp.goushuyun.com',
-//     base_url: 'http://admin.goushuyun.com/',
-//     home_href: 'http://admin.goushuyun.com/#'
-// }
+export default {
+    component_appid: 'wx6d36779ce4dd3dfa',
+    url_encode_domain: 'https%3a%2f%2fapp.goushuyun.com',
+    base_url: 'http://admin.goushuyun.com/',
+    home_href: 'http://admin.goushuyun.com/#'
+}
 
 // 开发环境
 export default {
+    component_appid: 'wxb39264d954771192',
     url_encode_domain: 'https%3a%2f%2fapp.goushuyun.cn',
     base_url: 'https://admin.goushuyun.cn/',
     home_href: 'https://admin.goushuyun.cn/#'
--- src/pages/store_info/weixin.vue
+++ src/pages/store_info/weixin.vue
@@ -233,7 +233,7 @@ export default {
             },
             store_url() {
                 // url 中填入官方服务号的 appid, 商户的store_id
-                return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6d36779ce4dd3dfa&redirect_uri='+ conf.url_encode_domain +'%2fone%2ftwo%2findex.html%23%2findex%2fmain&response_type=code&scope=snsapi_base&state=' + this.office_account.store_id + '&component_appid=wx1c2695469ae47724#wechat_redirect'
+                return 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6d36779ce4dd3dfa&redirect_uri='+ conf.url_encode_domain +'%2fone%2ftwo%2findex.html%23%2findex%2fmain&response_type=code&scope=snsapi_base&state=' + this.office_account.store_id + '&component_appid='+ conf.component_appid +'#wechat_redirect'
             }
         },
         mounted(){
