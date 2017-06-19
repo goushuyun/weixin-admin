760430e2d435e708548d6b3d8292b093e253e959
Wang-Kai <persistence2013@sina.com>
2017-06-19 14:34:25 +0800

    用脚本自动写入环境配置参数

--- publish.sh
+++ publish.sh
@@ -27,6 +27,16 @@ buildfile() {
 
 upload_test() {
     echo '即将发布到【测试】环境...'
+
+    # 写入配置文件
+    echo "export default {
+        redirect_uri: 'https://admin.goushuyun.cn/static/weixin_redirect.html',
+        component_appid: 'wxb39264d954771192',
+        url_encode_domain: 'https%3a%2f%2fapp.goushuyun.cn',
+        base_url: 'https://admin.goushuyun.cn/',
+        home_href: 'https://admin.goushuyun.cn/#'
+    }" > ./src/config/conf.js
+
     buildfile
     rsync -e ssh -r ./build/* --progress -cv 'root@115.28.109.6:/home/web/admin/'
     rsync -e ssh -r ./static/* --progress -cv 'root@115.28.109.6:/home/web/admin/static/'
@@ -35,6 +45,15 @@ upload_test() {
 
 upload_production() {
     echo '即将发布到【生产】环境...'
+
+    echo "export default {
+        redirect_uri: 'http://weixin.goushuyun.com/static/weixin_redirect.html',
+        component_appid: 'wx1c2695469ae47724',
+        url_encode_domain: 'https%3a%2f%2fapp.goushuyun.com',
+        base_url: 'http://admin.goushuyun.com/',
+        home_href: 'http://admin.goushuyun.com/#'
+    }" > ./src/config/conf.js
+
     buildfile
     rsync -e ssh -r ./build/* --progress -cv 'root@139.129.227.229:/home/web/admin'
     rsync -e ssh -r ./static/* --progress -cv 'root@139.129.227.229:/home/web/admin/static/'
