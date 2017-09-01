f582fef6f32992bd1775aa28a283ab8af8543d03
Wang-Kai <persistence2013@sina.com>
2017-09-01 09:32:04 +0800

    modify

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,6 +1,6 @@
 export default {
         redirect_uri: 'https://admin.goushuyun.cn/static/weixin_redirect.html',
-        component_appid: 'wx1c2695469ae47724',
+        component_appid: 'wxb39264d954771192',
         url_encode_domain: 'https%3a%2f%2fapp.goushuyun.cn',
         base_url: 'https://admin.goushuyun.cn/',
         home_href: 'https://admin.goushuyun.cn/#'
